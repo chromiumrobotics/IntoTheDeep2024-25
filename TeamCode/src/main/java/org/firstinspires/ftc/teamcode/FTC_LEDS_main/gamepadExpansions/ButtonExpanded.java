53cc3dd31960d7a0df4d7edb4f15d8a4623f94f2
Isaac-Pruett <isaacpruett@Isaacs-MacBook-Air.local>
2024-09-13 15:21:27 -0700

    Imported FTC-Addons Library. It's my Library for like the ButtonExpanded class and the NeoDriver. ButtonExpanded is super useful for Gamepad "debouncing" and fixing other bugs.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_LEDS_main/gamepadExpansions/ButtonExpanded.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.FTC_LEDS_main.gamepadExpansions;
+
+public class ButtonExpanded {
+    private boolean prevValue = false;
+    private boolean toggled  = false;
+
+    public void updateButtonValue(boolean newValue){
+        if (newValue && isChanged(newValue)) {
+            toggled = !toggled;
+        }
+
+        prevValue = newValue;
+    }
+
+    public boolean hasChangedToTrue(boolean buttonValue){
+        return buttonValue && isChanged(buttonValue);
+    }
+    public boolean hasChangedToFalse(boolean buttonValue){
+        return !buttonValue && isChanged(buttonValue);
+    }
+    public boolean isChanged(boolean buttonValue){
+        return buttonValue != prevValue;
+    }
+    public boolean isToggled(){
+        return toggled;
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_LEDS_main/tests_and_examples/NeopixelTest.java
@@ -0,0 +1,63 @@
+package org.firstinspires.ftc.teamcode.FTC_LEDS_main.tests_and_examples;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.FTC_LEDS_main.AdafruitNeodriver;
+
+
+@TeleOp
+//@Disabled
+public class NeopixelTest extends LinearOpMode {
+
+    AdafruitNeodriver neo;
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize_opmode();
+
+        waitForStart();
+        if (opModeIsActive()) {
+            while (opModeIsActive() && !isStopRequested()) {
+
+
+
+                int WRGB = 0x333333;
+                for (int i = 0; i < 12; i++){
+                    neo.setColor(WRGB, (short) i);
+                }
+
+                sleep(1000);
+                neo.clearAllPixels();
+                sleep(1000);
+
+                int red = ((WRGB >> (8*2)) & 0xfe);
+                int green = ((WRGB >> (8*1)) & 0xfe);
+                int blue = ((WRGB >> (8*0)) & 0xfe);
+                int white = ((WRGB >> (8*3)) & 0xfe);
+
+                telemetry.addData("red = ", red);
+                telemetry.addData("green = ", green);
+                telemetry.addData("blue = ", blue);
+                telemetry.addData("white = ", white);
+
+
+                telemetry.update();
+
+            }
+
+
+        }
+
+    }
+
+    public void initialize_opmode(){
+        neo = hardwareMap.get(AdafruitNeodriver.class, "neopixels");
+        neo.setPixelType(AdafruitNeodriver.ColorOrder.NEO_GRB);
+        neo.init_neopixels();
+
+    }
+}
