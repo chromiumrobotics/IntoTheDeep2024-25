66b79e658a29b3f60e792b17a554847cf0b05631
Isaac-Pruett <isaacpruett@Isaacs-Air.lan>
2024-10-25 20:33:25 -0700

    pushing small QOL update to ButtonExpanded.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_LEDS_main/gamepadExpansions/ButtonExpanded.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FTC_LEDS_main/gamepadExpansions/ButtonExpanded.java
@@ -1,15 +1,33 @@
 package org.firstinspires.ftc.teamcode.FTC_LEDS_main.gamepadExpansions;
 
 public class ButtonExpanded {
+    private boolean value = false;
     private boolean prevValue = false;
     private boolean toggled  = false;
 
+    public boolean isPressed(){
+        return value;
+    }
+
     public void updateButtonValue(boolean newValue){
+        value = newValue;
         if (newValue && isChanged(newValue)) {
             toggled = !toggled;
         }
 
         prevValue = newValue;
+    }
+
+
+    public ButtonExpanded(){
+
+    }
+    public boolean hasJustBeenPressed(boolean buttonValue){
+        return hasChangedToTrue(buttonValue);
+    }
+
+    public boolean hasJustBeenReleased(boolean buttonValue){
+        return hasChangedToFalse(buttonValue);
     }
 
     public boolean hasChangedToTrue(boolean buttonValue){
